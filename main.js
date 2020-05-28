64f90f40346359c53dcd0418e2358a3ac90bffab
rpcach <rpcach@users.noreply.github.com>
2020-05-27 22:47:42 -0400

    now generates a random alphanumeric character

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-function get_random_number() {
-	var random_number = Math.floor(Math.random()*10)
+function get_random_alphanumeric() {
+	var crypto = window.crypto
 
-	return random_number
+	var randomValue = new Uint8Array(1)
+	randomValue[0] = 64
+
+	while(randomValue[0] > 62) {
+		crypto.getRandomValues(randomValue)
+		randomValue[0] = (randomValue[0] % 64 + 1)
+	}
+	randomValue = randomValue[0]
+
+	if(randomValue >= 1 & randomValue <= 10) {
+		randomValue = String.fromCharCode(47 + randomValue)
+	} else if(randomValue >= 11 & randomValue <= 36) {
+		randomValue = String.fromCharCode(54 + randomValue)
+	} else if(randomValue >= 37 & randomValue <= 62) {
+		randomValue = String.fromCharCode(60 + randomValue)
+	}
+
+	return randomValue
 }
 
 function display_random_number() {
 	var string = "<pre>"
 	
-	for (var i = 1; i <= get_random_number()+1; i++) {
-		string += get_random_number()
+	for (var i = 0; i < 20; i++) {
+		string += get_random_alphanumeric() + " "
 	}
 	string += "</pre>"
 
