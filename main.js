cdda18ae98da9bc9a652a3d627d2eeafb3ecea29
rpcach <rpcach@users.noreply.github.com>
2020-05-27 23:18:33 -0400

    now generates an alphanumeric in a much simpler (and more understandable) way

--- main.js
+++ main.js
@@ -2,32 +2,22 @@ function get_random_alphanumeric() {
 	var crypto = window.crypto
 
 	var randomValue = new Uint8Array(1)
-	randomValue[0] = 64
 
-	while(randomValue[0] > 62) {
+	while(!String.fromCharCode(randomValue[0]).match(/^[a-zA-Z0-9]$/)) {
 		crypto.getRandomValues(randomValue)
-		randomValue[0] = (randomValue[0] % 64 + 1)
 	}
-	randomValue = randomValue[0]
 
-	if(randomValue >= 1 & randomValue <= 10) {
-		randomValue = String.fromCharCode(47 + randomValue)
-	} else if(randomValue >= 11 & randomValue <= 36) {
-		randomValue = String.fromCharCode(54 + randomValue)
-	} else if(randomValue >= 37 & randomValue <= 62) {
-		randomValue = String.fromCharCode(60 + randomValue)
-	}
+	randomValue = String.fromCharCode(randomValue[0])
 
 	return randomValue
 }
 
-function display_random_number() {
-	var string = "<pre>"
+function display_random_alphanumeric() {
+	var string = '<br>'
 	
 	for (var i = 0; i < 20; i++) {
-		string += get_random_alphanumeric() + " "
+		string += '<pre style="display:inline; padding-right:8px;">' + get_random_alphanumeric() + "</pre>"
 	}
-	string += "</pre>"
 
-	document.getElementById("random number").innerHTML = string
+	document.getElementById("random alphanumeric").innerHTML = string
 }
