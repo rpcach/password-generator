4f1f1081f84c00e53a648e56bd7387b10399875a
rpcach <rpcach@users.noreply.github.com>
2020-05-28 22:26:55 -0400

    Added functionality to select the length of the password and to copy the password

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function get_random_alphanumeric() {
 
 	var randomValue = new Uint8Array(1)
 
-	while(!String.fromCharCode(randomValue[0]).match(/^[a-zA-Z0-9]$/)) {
+	while(!String.fromCharCode(randomValue[0]).match(/[a-zA-Z0-9]/)) {
 		crypto.getRandomValues(randomValue)
 	}
 
@@ -12,12 +12,30 @@ function get_random_alphanumeric() {
 	return randomValue
 }
 
-function display_random_alphanumeric() {
-	var string = '<br>'
+function display_random_alphanumeric(val) {
+	var string = '<p style="font-family:Courier New; display:inline; font-size:25px; font-weight:bold; letter-spacing:7px; word-wrap:break-word;">'
 	
-	for (var i = 0; i < 20; i++) {
-		string += '<pre style="display:inline; padding-right:8px;">' + get_random_alphanumeric() + "</pre>"
+	for (var i = 0; i < val; i++) {
+		x = get_random_alphanumeric()
+		if(x.match(/[0-9]/)) {
+			string += '<span style="color:blue;">' + x + "</span>"
+		} else {
+			string += x
+		}
 	}
 
+	string += '</p>'
+
 	document.getElementById("random alphanumeric").innerHTML = string
+}
+
+function CopyToClipboard(containerid) {
+	if (window.getSelection) {
+		var range = document.createRange();
+		range.selectNode(document.getElementById(containerid));
+		window.getSelection().addRange(range);
+		document.execCommand("copy");
+		window.getSelection().removeAllRanges();
+		
+	}
 }
